f47341460aa963a40b77f5a6966a6bb4c936f92a
Felipe Poletti <felipepolettig@gmail.com>
2023-03-08 21:12:49 -0300

    Inserindo CSS no meu app React

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,7 +1,9 @@
+import styles from './Pessoa.module.css'
+
 function Pessoa({ foto, fotoDescricao, nome, idade, profissao }) {
   return(
     <div>
-      <img src={foto} alt={fotoDescricao}/>
+      <img  className={styles.img} src={foto} alt={fotoDescricao}/>
       <h2>Nome: {nome}</h2>
       <p>Idade: {idade}</p>
       <p>Profissão: {profissao}</p>
